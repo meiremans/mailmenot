9364ba4e53ca3bbc656fe2c1322942c9c44cd2e9
Nick <nick@ab-it.io>
2021-10-22 23:45:30 +0200

    who invents to send messages in URL's ??????????????

--- services/telegram.js
+++ services/telegram.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
 exports.sendMessage = async (message, chatId) => {
-    const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT}/sendMessage?chat_id=${chatId}&text=${message}`;
+    const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
     await axios.get(url);
 }
