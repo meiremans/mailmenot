7cb06e44fbeece6fcb91ca0b1b6ef1b31cb3e665
Nick <nick@ab-it.io>
2021-10-29 17:57:45 +0200

    only send html as parsemode html
    Signed-off-by: Nick <nick@ab-it.io>

--- services/telegram.js
+++ services/telegram.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
-exports.sendMessage = async (message, chatId) => {
+exports.sendMessage = async (message, chatId, parseMode) => {
     try{
-        const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}&parse_mode=html`;
+        const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}${parseMode ? "&parse_mode=html" : "" }`;
         await axios.get(url);
         console.log("message send:");
         console.log(message);
